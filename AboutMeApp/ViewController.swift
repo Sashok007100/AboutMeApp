1077756f6cddbac3017b2d51b21de7a690ad4155
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-06-02 21:34:05 +0300

    Initial Commit

---
+++ AboutMeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMeApp
+//
+//  Created by Alexandr Artemov (Mac Mini) on 02.06.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
