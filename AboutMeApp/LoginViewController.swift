c810aaef0f5c91f63bdc6f26e4f92fe3e779b34e
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-06-03 20:22:30 +0300

    Была создана логика экрана Login View Controller в соответствии с д/з.

---
+++ AboutMeApp/LoginViewController.swift
@@ -0,0 +1,85 @@
+import UIKit
+
+final class LoginViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet var usernameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    // MARK: - Private Properties
+    private let user = "Aleksandr"
+    private let password = "1"
+    
+    // MARK: - Overrides
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeVC = segue.destination as? WelcomeViewController
+        
+        welcomeVC?.displayedUsername = usernameTextField.text
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    override func shouldPerformSegue(
+        withIdentifier identifier: String,
+        sender: Any?
+    ) -> Bool {
+        guard usernameTextField.text == user,
+              passwordTextField.text == password else {
+            showAlert(
+                withTitle: "Invalid login or password",
+                andMessage: "Please, enter correct login and password",
+                shouldClearField: true
+            )
+            return false
+        }
+        return true
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func forgotUsernameButtonTapped() {
+        showAlert(
+            withTitle: "Oops!",
+            andMessage: "Your name is \(user) 😵‍💫",
+            shouldClearField: false
+        )
+    }
+    
+    @IBAction func forgotPasswordButtonTapped() {
+        showAlert(
+            withTitle: "Oops!",
+            andMessage: "Your password is \(password) 🫣",
+            shouldClearField: false
+        )
+    }
+    
+    @IBAction func unwind(_: UIStoryboardSegue) {
+        usernameTextField.text = ""
+        passwordTextField.text = ""
+        
+    }
+    
+    // MAKR: - Private Methods
+    private func showAlert(
+        withTitle title: String,
+        andMessage message: String,
+        shouldClearField: Bool
+    ) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            if shouldClearField {
+                self.passwordTextField.text = ""
+            }
+        }
+        alert.addAction(okAction)
+        
+        present(alert, animated: true)
+    }
+}
+
