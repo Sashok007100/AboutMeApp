cf2108fc095d5cec0f323023b50cda07b424799e
Artemov Aleksandr <sasha.artemov.68@mail.ru>
2025-06-08 02:22:48 +0300

    Добавлена модель User с вложенной структурой Person. Так же была добавлена тестовая фотография.

---
+++ AboutMeApp/Models/User.swift
@@ -0,0 +1,37 @@
+//
+//  User.swift
+//  AboutMeApp
+//
+//  Created by Alexandr Artemov (Mac Mini) on 08.06.2025.
+//
+
+struct User {
+    let username: String
+    let password: String
+    let person: Person
+    
+    static func getMockResponse() -> User {
+        User(
+            username: "Aleksandr",
+            password: "1",
+            person: Person(
+                title: "Александр Артемов",
+                image: "IMG_0720",
+                firstName: "Александр",
+                secondName: "Артемов",
+                city: "Москва",
+                bio: "Мне 26 лет. Родился и живу в Москве. В 2021 году окончил РТУ МИРЭА по направлению \"Программная инженерия\". В 2022 году устроился работать на склад на Горбушке. В 2024 году сменил работу, и теперь работаю в Президенской Академии (ранее РАНХиГС)."
+            )
+        )
+    }
+
+}
+
+struct Person {
+    let title: String
+    let image: String
+    let firstName: String
+    let secondName: String
+    let city: String
+    let bio: String
+}
